5266c5286068bfc9f6bf7b2d0d7cd3ba7615b624
tokyokzzzz <your_email@domain.com>
2025-10-08 12:43:36 +0500

    third commit

--- practice.go
+++ practice.go
@@ -1,13 +1,19 @@
 package main 
 import ("fmt")
 func main () {
-	fmt.Println("go"+"lang")
-	fmt.Println("1+1=",1+1)
-	fmt.Println("7.0/3.0=",7.0/3.0)
-	fmt.Println(true&&false)
-	fmt.Println(true||false)
-	fmt.Println(!true)
+	var a = "initial"
+	fmt.Println(a)
 
+	var b , c int =1,3
+	fmt.Println(b,c) 
 
+	var d = true
+	fmt.Println(d)
 
+	var e int
+	fmt.Println(e)
+
+	f := "Apple"
+	fmt.Println(f)
 }
+
